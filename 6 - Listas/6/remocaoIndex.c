a6d4cfee8798712ff91d206261b2991f4705f3b2
Dutra-fran <sgtrobsondutra51@gmail.com>
2023-03-27 19:19:20 -0300

    Listas - Remoção

---
+++ 6 - Listas/6/remocaoIndex.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct ponto
+{
+    float x;
+    float y;
+    struct ponto *prox;
+};
+
+typedef struct ponto Ponto;
+
+Ponto *listaPontos; // aponta para o início da lista
+
+int length = 0;
+
+void addIndex(float x, float y, int index)
+{
+    Ponto *p = (Ponto *)malloc(sizeof(Ponto));
+    p->x = x;
+    p->y = y;
+
+    if (index > length)
+    {
+        printf("Posição inválida!\n");
+    }
+    else
+    {
+        if (index == 0)
+        {
+            p->prox = listaPontos;
+            listaPontos = p;
+        }
+        else
+        {
+            Ponto *auxLista = listaPontos;
+            int i = 0;
+            while (i != index - 1)
+            {
+                auxLista = auxLista->prox;
+                i++;
+            }
+            p->prox = auxLista->prox;
+            auxLista->prox = p;
+        }
+        length++;
+    }
+}
+
+void removeIndex(int index)
+{
+    if (index > length || length == 0)
+    {
+        printf("Posição inválida ou lista vazia!\n");
+    }
+    else
+    {
+        if (index == 0)
+        {
+            listaPontos = listaPontos->prox;
+        }
+        else
+        {
+            Ponto *auxLista = listaPontos;
+            int i = 0;
+            while (i != index - 1)
+            {
+                auxLista = auxLista->prox;
+                i++;
+            }
+            auxLista->prox = auxLista->prox->prox;
+        }
+        length--;
+    }
+}
+
+void imprime(Ponto *p)
+{
+    if (p != NULL)
+    {
+        printf("Ponto(%.1f, %.1f)\n", p->x, p->y);
+        imprime(p->prox);
+    }
+}
+
+int main(void)
+{
+    removeIndex(0);
+
+    addIndex(1, 1, 0);
+    addIndex(2, 4, 1);
+    addIndex(4, 16, 2);
+    addIndex(3, 9, 2);
+    addIndex(0, 0, 0);
+
+    removeIndex(2);
+
+    Ponto *auxLista = listaPontos;
+    imprime(auxLista);
+
+    return 0;
+}
