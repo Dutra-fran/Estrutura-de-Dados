75b096d4f6979e893d764b25f46daae3756edf1c
Dutra-fran <sgtrobsondutra51@gmail.com>
2023-04-06 20:37:19 -0300

    Consulta na Lista Dinâmica Encadeada

---
+++ 6 - Listas/16/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ListaDinEncad.h"
+
+int main(void)
+{
+    Lista *li;
+    li = cria_lista();
+
+    // int x = lista_cheia(li);
+    // if(lista_cheia(li))
+
+    // int x = lista_vazia(Lista *li);
+    // if(lista_vazia(li))
+
+    // int x = insere_lista_inicio(li, dados_aluno);
+    // int x = insere_lista_final(li, dados_aluno);
+    // int x = insere_lista_ordenada(li, dados_aluno);
+
+    // int x = remove_lista_inicio(li);
+    // int x = remove_lista_final(li);
+    // int x = remove_lista(li, matricula_aluno);
+
+    // int x = consulta_lista_pos(li, posicao, &dados_aluno);
+    // int x = consulta_lista_mat(li, mat, &dados_aluno);
+
+    libera_lista(li);
+
+    return 0;
+}
