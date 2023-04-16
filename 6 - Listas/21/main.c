b4059ab26633c08eb583fd9364e468810f37bca6
Dutra-fran <sgtrobsondutra51@gmail.com>
2023-04-16 15:22:11 -0300

    Consulta em Lista Duplamente Encadeada

---
+++ 6 - Listas/21/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "ListaDinEncadDupla.h"
+
+int main(void)
+{
+    Lista *li;
+    li = cria_lista();
+
+    // int x = tamanho_lista(li);
+    // int x = lista_cheia(li);
+    // if(lista_cheia(li))
+    // int x = lista_vazia(li);
+    // if(lista_vazia(li))
+
+    // int x = insere_lista_inicio(li, dados_aluno);
+    // int x = insere_lista_final(li, dados_aluno);
+    // int x = insere_lista_ordenada(li, dados_aluno);
+
+    // int x = remove_lista_inicio(li);
+    // int x = remove_lista_final(li);
+    // int x = remove_lista(li, matricula_aluno);
+
+    // int x = consulta_lista_pos(li, posicao, &dados_aluno);
+    // int x = consulta_lista_mat(li, posicao, &dados_aluno);
+
+    libera_lista(li);
+
+    return 0;
+}
